4bf3e1056e11b05255b6a7c66de04af92974a38d
Hazal Sendogan <sendoganhazal@gmail.com>
2026-03-17 14:40:09 +0300

    responsive cart overlay

--- src/components/atoms/CartItem.tsx
+++ src/components/atoms/CartItem.tsx
@@ -7,7 +7,7 @@ const CartItem = ({ item }: { item: Item }) => {
     const { removeFromCart, increaseQty, decreaseQty } = useCart();
 
     return (
-        <Card.Root flexDirection="row" overflow="hidden" flexGrow={"1"} marginBottom={"1.25rem"}>
+        <Card.Root flexDirection={{base:"column", md:"row"}} overflow="hidden" flexGrow={"1"} marginBottom={"1.25rem"}>
             <Image
                 objectFit="cover"
                 maxW="200px"
@@ -17,18 +17,21 @@ const CartItem = ({ item }: { item: Item }) => {
 
             <Card.Body>
                 <Card.Title mb="2">{item.title}</Card.Title>
-                <Card.Description>
-                    <Flex alignItems={"center"} gap={2} marginBottom={"1rem"}>
+                <Card.Description >
+                    <Flex flexDirection={{base:"row", md:"column"}} justifyContent={{base:"space-between", md:"flex-start"}} >
+<Flex   alignItems={"center"} gap={2} marginBottom={"1rem"}>
                         <Button variant={"outline"} colorPalette={"pink"} onClick={() => decreaseQty(item.id)}>-</Button>
                         <Text color={"gray.800"} fontWeight={"medium"}>{item.quantity}</Text>
                         <Button variant={"outline"} colorPalette={"pink"} onClick={() => increaseQty(item.id)}>+</Button>
                        
                     </Flex>
-                     <Text textStyle={"lg"} color={"cyan.500"} fontWeight={"medium"}> ${(item.price * item.quantity).toFixed(2)}</Text>
+                    <Text textStyle={"lg"} color={"cyan.500"} fontWeight={"medium"}> ${(item.price * item.quantity).toFixed(2)}</Text>
+                    </Flex>
+                    
                 </Card.Description>
 
             </Card.Body>
-            <Card.Footer>
+            <Card.Footer justifyContent={{base:"flex-end", md:"flex-start"}}>
                 <Button variant="outline" colorPalette={"red"} onClick={() => removeFromCart(item.id)}>Remove Product</Button>
             </Card.Footer>
         </Card.Root>
