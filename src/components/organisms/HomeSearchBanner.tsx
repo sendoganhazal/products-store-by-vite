a93c46793c0b1ae2754dae61997e771b7382aff9
Hazal Sendogan <sendoganhazal@gmail.com>
2026-03-17 14:11:28 +0300

    greeting banner

--- src/components/organisms/HomeSearchBanner.tsx
+++ src/components/organisms/HomeSearchBanner.tsx
@@ -1,6 +1,13 @@
+import { Container, Flex,Heading } from "@chakra-ui/react";
 const HomeSearchBanner = () => {
   return (
-    <section>HomeSearchBanner</section>
+    <section className="banner">
+      <Container>
+        <Flex alignItems={"center"} justifyContent={"center"} >
+          <Heading textStyle={"6xl"}>This project is built with React & Vite. Data provided by DummyJSON.</Heading>
+        </Flex>
+      </Container>
+    </section>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,9 @@
-import { Container, Image} from "@chakra-ui/react";
-import StayTuned from "../../assets/stay_tuned.jpg"
+
+import HomeSearchBanner from "../../components/organisms/HomeSearchBanner";
+// import StayTuned from "../../assets/stay_tuned.jpg"
 
 const Home = () => (
-  <Container>
-    <Image src={StayTuned} alt="Stay Tuned"/>
-  </Container>
+  <HomeSearchBanner />
 );
 
 
